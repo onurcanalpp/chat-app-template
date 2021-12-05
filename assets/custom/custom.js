83f45e04b9e689dcfd3a3f207652b54bac0e752a
onurcanalpp <onurcanalpp@gmail.com>
2021-12-05 18:39:10 +0300

    chat home tab menu added and some scss changes

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,5 +1,18 @@
 
 
-console.log(respData);
 
-getUsers();
+const homeTabItem = document.querySelectorAll(".home-tab-list li");
+
+function clearHomeTabClass(){
+    for(let i = 0; i<homeTabItem.length; i++){
+        homeTabItem[i].classList.remove("active");
+    }
+}
+
+for(let i = 0; i < homeTabItem.length; i++){
+    
+    homeTabItem[i].addEventListener("click", function(){
+        clearHomeTabClass();
+        this.classList.add("active");
+    });
+}
