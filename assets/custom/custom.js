1eece3b935d25cc1981a2923ebdaf21a0c7c37f7
onurcanalpp <onurcanalpp@gmail.com>
2021-12-05 22:06:33 +0300

    message chat panel added

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -10,9 +10,31 @@ function clearHomeTabClass(){
 }
 
 for(let i = 0; i < homeTabItem.length; i++){
-    
     homeTabItem[i].addEventListener("click", function(){
         clearHomeTabClass();
         this.classList.add("active");
     });
 }
+
+
+
+function getBaseUrl(segment){
+    // get the segments
+    pathArray = window.location.pathname.split( '/' );
+    // find where the segment is located
+    indexOfSegment = pathArray.indexOf(segment);
+    // make base_url be the origin plus the path to the segment
+    var baseUrl = window.location.origin + pathArray.slice(0,indexOfSegment).join('/') + '/';
+    return baseUrl;
+}
+
+
+
+function redirectToDetail(){
+    window.location = getBaseUrl() + "messageDetail.html";
+}
+
+
+
+ 
+ 
